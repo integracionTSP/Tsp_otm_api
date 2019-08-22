c53420ae91d524de83a8d762d8e90303e669153f
Anthony Henriquez <ajhen217@gmail.com>
2019-08-22 17:35:13 -0500

    add upepercase in power_unit

--- config/database/conexiondb.js
+++ config/database/conexiondb.js
@@ -1,5 +1,19 @@
 const pg = require('pg');
 
+
+
+module.exports = () => {
+    return new pg.Pool({
+
+        user: 'api',
+        host: '172.16.3.222',
+        database: 'sot',
+        password: 'api2019',
+        port: 5432
+
+    });
+}
+/*
 module.exports = () => {
     return new pg.Pool({
 
@@ -11,3 +25,5 @@ module.exports = () => {
 
     });
 }
+*/
+
