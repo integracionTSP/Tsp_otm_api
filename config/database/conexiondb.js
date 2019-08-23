f2b0575932d5fc3ef551d314e7be14cf34b39109
anthonyortiz <adjog96@gmail.com>
2019-08-23 16:00:56 -0500

    ++

--- config/database/conexiondb.js
+++ config/database/conexiondb.js
@@ -2,12 +2,24 @@ const pg = require('pg');
 
 module.exports = () => {
     return new pg.Pool({
-
+        
+        user: 'api',
+        host: '172.16.3.222',
+        database: 'sot',
+        password: 'api2019',
+        port: 5432
+        /*
         user: 'ahenriquez',
         host: '172.16.3.222',
         database: 'sot',
         password: 'ah2019',
         port: 5432
 
+        user: 'aortiz',
+        host: '172.16.3.222',
+        database: 'sot',
+        password: 'tsp2019',
+        port: 5432
+*/
     });
 }
