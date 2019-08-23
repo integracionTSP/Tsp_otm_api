6ffa73890c637cf3b96e64bac656a2820d385fc7
anthonyortiz <adjog96@gmail.com>
2019-08-23 16:07:38 -0500

    ++

--- config/database/conexiondb.js
+++ config/database/conexiondb.js
@@ -1,19 +1,4 @@
 const pg = require('pg');
-
-
-
-module.exports = () => {
-    return new pg.Pool({
-
-        user: 'api',
-        host: '172.16.3.222',
-        database: 'sot',
-        password: 'api2019',
-        port: 5432
-
-    });
-}
-/*
 module.exports = () => {
     return new pg.Pool({
         
@@ -37,5 +22,5 @@ module.exports = () => {
 */
     });
 }
-*/
+
 
